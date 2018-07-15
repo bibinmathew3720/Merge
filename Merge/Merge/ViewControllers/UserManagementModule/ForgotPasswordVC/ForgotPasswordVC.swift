12ff92ef224ff90ab355bc561bfd8b1c3857392f
Bibin Mathew <bibin.m@codelynks.com>
2018-07-15 14:18:17 +0530

    Forgot password api integration.

--- Merge/Merge/ViewControllers/UserManagementModule/ForgotPasswordVC/ForgotPasswordVC.swift
+++ Merge/Merge/ViewControllers/UserManagementModule/ForgotPasswordVC/ForgotPasswordVC.swift
@@ -12,6 +12,7 @@ class ForgotPasswordVC: BaseViewController,UITextFieldDelegate {
     @IBOutlet weak var emailView: UIView!
     @IBOutlet weak var emailTF: UITextField!
     @IBOutlet weak var backToLogInButton: UIButton!
+    var alwisalForgotPassword = AlwisalForgotPassword()
     
     override func initView() {
         super.initView()
@@ -47,12 +48,71 @@ class ForgotPasswordVC: BaseViewController,UITextFieldDelegate {
     @IBAction func backToLogInButtonAction(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
+    @IBAction func resetPasswordButtonAction(_ sender: UIButton) {
+        if (validate()){
+            callingForgotPasswordApi()
+        }
+    }
     
     //MARK: Text Field Delegates
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    //MARK: validation
+    
+    func validate()->Bool{
+        var valid = true
+        var messageString = ""
+        if (emailTF.text?.isEmpty)!{
+            messageString = "Please enter your email id"
+            valid = false
+        }
+        else if !(emailTF.text?.isValidEmail())!{
+            messageString = "Please enter valid email id"
+            valid = false
+        }
+        if valid == false{
+            AlwisalUtility.showDefaultAlertwith(_title: Constant.AppName, _message: messageString, parentController: self)
+        }
+        return valid
+    }
+    
+    // MARK: Calling Forgot Password Api
+    
+    func  callingForgotPasswordApi(){
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        UserModuleManager().callingForgotPasswordApi(with: getRequestBody(), success: {
+            (model) in
+            MBProgressHUD.hide(for: self.view, animated: true)
+            if let model = model as? AlwisalForgotResponseModel{
+                if model.errorCode == 1{
+                    AlwisalUtility.showDefaultAlertwith(_title: Constant.AppName, _message: model.errorMessage, parentController: self)
+                }
+                else{
+                    AlwisalUtility.showDefaultAlertwithCompletionHandler(_title: Constant.AppName, _message: model.statusMessage, parentController: self, completion: { (okSuccess) in
+                        self.navigationController?.popViewController(animated: true)
+                    })
+                }
+                
+            }
+            
+        }) { (ErrorType) in
+            MBProgressHUD.hide(for: self.view, animated: true)
+            if(ErrorType == .noNetwork){
+                AlwisalUtility.showDefaultAlertwith(_title: Constant.AppName, _message: Constant.ErrorMessages.noNetworkMessage, parentController: self)
+            }
+            else{
+                AlwisalUtility.showDefaultAlertwith(_title: Constant.AppName, _message: Constant.ErrorMessages.serverErrorMessamge, parentController: self)
+            }
+        }
+    }
+    
+    fileprivate func getRequestBody() -> String {
+        alwisalForgotPassword.email = emailTF.text!
+        return alwisalForgotPassword.getRequestBody()
     }
     
     /*
